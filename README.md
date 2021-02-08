5fa3d6f10bcec24eca8fa601ff4634925c058743
Larsen <larsenclose@protonmail.com>
2021-02-08 01:46:43 -0700

    Please enter the commit message for your changes. Lines starting

--- README.md
+++ README.md
@@ -1,15 +1,47 @@
 # SumsToN
+
 Recursive SumsToN implementation written in Java
 
-
 ## Compile
+
 javac SumsToN.java
 
 ### Run
+
 java SumsToN
 
 #### Usage
+
 Enter any positive integer
 
 ##### Result
+
 All combinations of positive integers from 1-N which sum to N, will be displayed in non descdening order from left to right.
+
+```bash
+Combinatorial solutions for numbers 0 to N, which sum to N. Exclusively increasing order [left to right], without repetitions.
+Enter Integer N for combinatorial summations: 
+8
+1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 2 
+1 1 1 1 1 3 
+1 1 1 1 2 2 
+1 1 1 1 4 
+1 1 1 2 3 
+1 1 1 5 
+1 1 2 2 2 
+1 1 2 4 
+1 1 3 3 
+1 1 6 
+1 2 2 3 
+1 2 5 
+1 3 4 
+1 7 
+2 2 2 2 
+2 2 4 
+2 3 3 
+2 6 
+3 5 
+4 4 
+8 
+```
--- SumsToN.java
+++ SumsToN.java
@@ -8,21 +8,17 @@ import java.util.Scanner;
  *	Exercise 2
  */
 
-
-
-
 public class SumsToN {
-
 
    public static void main(String[] args) {
 
-      System.out.println("\n\nCombinatorial solutions for numbers 0 to N, which sum to N. Exclusively increasing order, without repetitions.");
+      System.out.println(
+            "\n\nCombinatorial solutions for numbers 0 to N, which sum to N. Exclusively increasing order [left to right], without repetitions.");
       Scanner scan = new Scanner(System.in);
 
       System.out.println("Enter Integer N for combinatorial summations: ");
 
       int n = scan.nextInt();
-
 
       sum_some_sums(n);
       scan.close();
@@ -30,55 +26,36 @@ public class SumsToN {
 
    }
 
-   private static void sum_some_sums(int n){
-      
-      int sums[] = new int [n];
- 
+   private static void sum_some_sums(int n) {
 
+      int sums[] = new int[n];
 
-
-
-      summing(sums, 0, n,n);      
+      summing(sums, 0, n, n);
    }
 
-   private static void summing( int sums[], int index, int starting, int last ) {
-      
- 
+   private static void summing(int sums[], int index, int starting, int last) {
 
-
-
-      if (last < 0 ) {
-         return;         
-      } 
-      if ( last ==  0) {
-               
-         for (int i = 0; i < index; i++) 
-            System.out.print (sums[i] + " ");
-            System.out.println(); 
-            return;  
-
+      if (last < 0) {
+         return;
       }
-            int previous;
+      if (last == 0) {
 
-            if( index == 0){
-               previous = 1; 
-            } else{
-               previous = sums[index-1];
-            }
+         for (int i = 0; i < index; i++)
+            System.out.print(sums[i] + " ");
+         System.out.println();
+         return;
+      }
+      int previous;
 
-            for(int i = previous; i <= starting ; i ++){
-                  sums[index] = i;
-                  summing( sums, index + 1,starting, last - i);
-         }
+      if (index == 0) {
+         previous = 1;
+      } else {
+         previous = sums[index - 1];
+      }
+
+      for (int i = previous; i <= starting; i++) {
+         sums[index] = i;
+         summing(sums, index + 1, starting, last - i);
       }
    }
-      
-
-
-
-
-
-
-
-
-
+}
